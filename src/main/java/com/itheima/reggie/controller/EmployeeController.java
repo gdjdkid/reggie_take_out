d92801f86ce506208c37fb6caa57cb9d8fabf830
Roy.Li <542002829@qq.com>
2023-10-31 01:10:16 +0800

    2023.10.30 3rd

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,11 +1,52 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
+import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+
+    /**
+     * 员工登录
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        String password = employee.getPassword();
+        //对明文的密码进行MD5加密处理
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        //查询条件，等值查询   封装好了用户名
+        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        //getOne方法用于查询一条数据 用户名是唯一的
+        Employee emp = employeeService.getOne(queryWrapper);
+        if (emp==null){
+            return R.error("登录失败！");
+        }
+
+        //该看P11集
+
+
+
+        return null;
+    }
 }

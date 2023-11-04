97095d7e17a0d7a3fb8e2c87b020d7fba5183099
Roy.Li <542002829@qq.com>
2023-11-04 23:43:35 +0800

    2023.11.04

--- src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -1,5 +1,7 @@
 package com.itheima.reggie.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.AntPathMatcher;
 
@@ -35,7 +37,10 @@ public class LoginCheckFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         String requestURI = request.getRequestURI();  //本次请求URI
-        String[] urls = new String[]{               //规定放行的路径
+
+        log.info("拦截到请求：{}", requestURI);  //用于debug追踪问题
+
+        String[] urls = new String[]{               //规定放行的路径集合
                 "/employee/login",
                 "/employee/logout",
                 "/backend/**",
@@ -43,18 +48,28 @@ public class LoginCheckFilter implements Filter {
         };
 
         boolean check = check(urls, requestURI);   // 判断本次请求URI是否需要放行
+        if (check) {                                 //如果不需要处理，就直接放行
+            log.info("本次请求{}不需要处理", requestURI);   //用于debug追踪问题
+            filterChain.doFilter(request, response);   //放行
+            return;
+        }
 
+        if (request.getSession().getAttribute("employee") != null) {   //判断登录状态
+            log.info("用户已登录，用户ID为:{}", request.getSession().getAttribute("employee"));   //用于debug追踪问题
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-        log.info("拦截到请求：{}", request.getRequestURI());
-        //先放行
-        filterChain.doFilter(request, response);
-
-
+        log.info("用户未登录！");  //用于debug追踪问题
+        //如果未登录，就返回未登录结果
+        //通过输出流方式向客户端页面响应数据
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
     }
 
     /**
-     * @param urls        规定放行的路径
-     * @param requestURI   本次请求URI
+     * @param urls       规定放行的路径集合
+     * @param requestURI 本次请求URI
      * @return boolean
      * @author Roy
      * @Description TODO
@@ -64,18 +79,12 @@ public class LoginCheckFilter implements Filter {
     public boolean check(String[] urls, String requestURI) {
         for (String url : urls) {
             boolean match = PATH_MATCHER.match(url, requestURI);
-            if (match){
+            if (match) {
                 return true;   // 匹配成功
             }
         }
         return false;   //匹配不上
     }
-
-
-
-
-
-
 
 
 }

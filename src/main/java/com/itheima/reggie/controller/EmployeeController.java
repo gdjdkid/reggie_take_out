0497d97ca51304fa10b9f0802cba177f66dc4633
Roy.Li <542002829@qq.com>
2023-11-09 23:48:00 +0800

    2023.11.09

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -29,7 +30,6 @@ public class EmployeeController {
      */
     @PostMapping("/login")
     public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
-
 
 
         String password = employee.getPassword();
@@ -64,6 +64,33 @@ public class EmployeeController {
     public R<String> logout(HttpServletRequest request) {
         request.getSession().removeAttribute("employee");
         return R.success("退出成功！");
+    }
+
+    /**
+     * @param employee
+     * @return com.itheima.reggie.common.R<java.lang.String>
+     * @author Roy
+     * @Description 新增员工
+     * @Date 2023/11/9 22:46
+     **/
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工，员工信息：{}", employee.toString());
+
+        //设置初始密码123456  需要MD5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        //LocalDateTime.now() 获取当前系统时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获得当前登录用户的ID
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+
+        return R.success("新增员工成功！");
     }
 
 

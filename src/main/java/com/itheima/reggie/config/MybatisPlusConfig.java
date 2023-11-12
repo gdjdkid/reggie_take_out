953c00fc4c2407b96f516cb77b877f552fbadf14
Roy.Li <542002829@qq.com>
2023-11-12 22:47:26 +0800

    2023.11.12   asdasdasd

---
+++ src/main/java/com/itheima/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,40 @@
+/*
+ * ----------------------------------------------------------------------------------------------------------
+ * Copyright (c) 2021-2023 AIA . All Rights Reserved.
+ * ----------------------------------------------------------------------------------------------------------
+ * Project Name: COAST_<Detail_Module_Name>
+ * @Author: Roy
+ * @Version: 1.0
+ * @Date: Created in 2023-11-12
+ * @Description: xxxxxxxxxxxxxxxxxxxxxxxxx
+ * ----------------------------------------------------------------------------------------------------------
+ * JIRA No               Programmer                  Date                  Description
+ * ----------------------------------------------------------------------------------------------------------
+ * xxxxxxxxx             xxxxxxxxxxxx                xxxxxxxx              xxxxxxxxxxxxx
+ * ----------------------------------------------------------------------------------------------------------
+ */
+
+package com.itheima.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+
+/**
+ * @ClassName MybatisPlusConfig
+ * @Description 配置MP的分页插件
+ * @Author Roy
+ * @Date 2023/11/12 22:44
+ * @Version 1.0
+ **/
+public class MybatisPlusConfig {
+
+    //拦截器
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+
+
+
+
+
+
+        return null;
+    }
+}
